4725fefa5dfbec2e5aefb3861d5597cd8aed8eda
HANNAH <HUPEE2001@YAHOO.COM>
2024-03-21 11:58:58 +0000

    updated

---
+++ app.js
@@ -0,0 +1,18 @@
+console.log("Hello Everyone");
+console.dir(document);
+
+//selecting an element
+
+const body=document.querySelector("body")
+
+console.log(body)
+
+const h1 = document.querySelector("h1")
+
+console.log("h1")
+
+//Modifying objects
+
+const h2=document.querySelector("h1")
+
+h1.textContent="My DOM  worksop";
